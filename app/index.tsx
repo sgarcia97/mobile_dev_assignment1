5cefad3e7615649c5c025532db672508fe8d2260
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-02-06 10:37:45 -0700

    Updates for main stylesheet and button component

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 
 import { StyleSheet, Text, View } from 'react-native';
+import styles from "../components/styles"
 
 export default function App() {
   return (
@@ -9,11 +10,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ components/Button.tsx
@@ -0,0 +1,18 @@
+import {Text, View, TouchableHighlight} from "react-native"
+import styles from "./styles"
+type ButtonProp = {
+    title:string;
+}
+const Button = (props:ButtonProp) => {
+    return(
+        <TouchableHighlight>
+            <View style={styles.button} >
+                <Text style={styles.buttontext}>{props.title}</Text>
+            </View>
+        </TouchableHighlight>
+
+    
+    )
+}
+
+export default Button
