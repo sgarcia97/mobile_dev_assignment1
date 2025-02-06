09fd62df4091c8c69a6a637f2b402c79a1fc53ed
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-02-06 13:01:34 -0700

    created input component

--- app/index.tsx
+++ app/index.tsx
@@ -2,11 +2,13 @@
 import { StyleSheet, Text, View } from 'react-native';
 import styles from "../components/styles"
 import Button from "../components/Button"
+import Input from "../components/Input"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Explore Canada</Text>
+      <Text style={styles.welcometitle}>Welcome to Explore Canada</Text>
+      <Input place="Username" label="Username"/>
       <Button title="Welcome"/>
     </View>
   );
--- components/styles.tsx
+++ components/styles.tsx
@@ -7,14 +7,38 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
       },
+      welcometitle:{
+        fontSize:40,
+        textAlign:"center",
+        fontWeight:600
+      },
       button:{
         paddingInline:20,
         paddingBlock:10,
         backgroundColor:"blue",
-        borderRadius:7
+        borderRadius:7,
+        width:150
       },
       buttontext:{
-        color:"#ffffff"
+        color:"#ffffff",
+        textAlign:"center"
+      },
+      inputWrapper:{
+        gap:5
+      },
+      input:{
+        height:40,
+        width:150,
+        borderRadius:5,
+        paddingInline:15,
+        backgroundColor:"#fff",
+        borderWidth:2,
+        borderColor:"#eaeaea"
+
+      },
+      label:{
+        fontSize:12,
+        color:"#999"
       }
 })
 
