82256ad5c7b69371a7bd829122c95596c423ee52
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-02-07 12:22:34 -0700

    removed option headers for tab screens

--- app/home/_layout.tsx
+++ app/home/_layout.tsx
@@ -4,13 +4,14 @@ const TabLayout = () => {
     return(
         <Tabs>
             <Tabs.Screen name="index" options={{
+                headerShown:false,
                 title:"Home"
             }}/>
             <Tabs.Screen name="calgary" options={{
-
+                headerShown:false
             }}/>
             <Tabs.Screen name="edmonton" options={{
-
+                headerShown:false
             }}/>
         </Tabs>
     )
