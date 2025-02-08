7f20d3f328809592d590edd9d5fc623be3d68eac
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-02-08 10:01:21 -0700

    Updated icons and page layout

--- app/home/_layout.tsx
+++ app/home/_layout.tsx
@@ -1,17 +1,29 @@
 import {Tabs} from 'expo-router'
-
+import Ionicons from '@expo/vector-icons/Foundation';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 const TabLayout = () => {
     return(
         <Tabs>
             <Tabs.Screen name="index" options={{
                 headerShown:false,
-                title:"Home"
-            }}/>
+                title:"Home",
+                tabBarIcon:({focused, color}) => {
+                    return <Ionicons name="home" size={24} color={color} />
+                }
+                }}/>
             <Tabs.Screen name="calgary" options={{
-                headerShown:false
+                headerShown:false,
+                title:"Calgary",
+                tabBarIcon:({focused, color}) => {
+                    return <FontAwesome6 name="mountain-city" size={18} color={color} />
+                }
             }}/>
             <Tabs.Screen name="edmonton" options={{
-                headerShown:false
+                headerShown:false,
+                title:"Edmonton",
+                tabBarIcon:({focused, color}) => {
+                    return <FontAwesome6 name="tree-city" size={18} color={color} />
+                }
             }}/>
         </Tabs>
     )
